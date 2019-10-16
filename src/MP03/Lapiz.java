76b41d98599d1d4b836c36ceef7882d9ad6e08ad
dam2a <ppatel77@elpuig.xeill.net>
2019-10-16 19:50:58 +0200

    lapiz all

--- src/MP03/Lapiz.java
+++ src/MP03/Lapiz.java
@@ -1,10 +1,12 @@
 package MP03;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 
 public class Lapiz implements Comparable<Lapiz>  {
     int color;
+
     int gruix;
 
     public Lapiz(int size, int color) {
@@ -12,8 +14,17 @@ public class Lapiz implements Comparable<Lapiz>  {
         this.color = color;
     }
 
-    public void setColor(int color) {
-        this.color = color;
+    public Lapiz() {
+        setColor();
+        setGruix();
+    }
+
+    public void setColor() {
+        this.color =  (int)(Math.random()*100)+1;
+    }
+
+    public void setGruix() {
+        this.gruix =  (int)(Math.random()*7)+1;
     }
 
     public int getColor() {
@@ -24,11 +35,19 @@ public class Lapiz implements Comparable<Lapiz>  {
         return gruix;
     }
 
-    public void setGruix(int gruix) {
-        this.gruix = gruix;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lapiz lapiz = (Lapiz) o;
+        return color == lapiz.color;
     }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(color);
+    }
 
     @Override
     public String toString() {
@@ -46,6 +65,16 @@ public class Lapiz implements Comparable<Lapiz>  {
     }
 }
 
+class Sortbycolor implements Comparator<Lapiz>{
+
+    @Override
+    public int compare(Lapiz t1, Lapiz t2) {
+        if (t1.getColor() > t2.getGruix()) return 1;
+        else if (t1.getColor() < t2.getColor()) return -1;
+        return 0;
+    }
+}
+
 class Sortbygruix implements Comparator<Lapiz>{
     @Override
     public int compare(Lapiz t1, Lapiz t2) {
@@ -54,53 +83,7 @@ class Sortbygruix implements Comparator<Lapiz>{
         return 0;
     }
 }
-//class SortbySize implements Comparator<Lapiz>{
-//
-//    @Override
-//    public int compare(Lapiz s1, Lapiz s2) {
-//        return s1.size.compareTo(s2.size);
-//    }
-//}
 
-
-
-
-//public class Lapiz implements Comparable<Lapiz>{
-//    int size;
-//    int color;
-//
-//    public Lapiz(int size, int color) {
-//        this.size = size;
-//        this.color = color;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Lapiz lapiz = (Lapiz) o;
-//        return size == lapiz.size &&
-//                color == lapiz.color;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(size, color);
-//    }
-//
-//    @Override
-//    public int compareTo(Lapiz lapiz) {
-//        if (this.equals(lapiz)) return 1;
-//        else return 0;
-//    }
-//
-//    public int getSize() {
-//        return size;
-//    }
-//
-//    public void setSize(int size) {
-//        this.size = size;
-//    }
 
 
 
--- src/MP03/Main.java
+++ src/MP03/Main.java
@@ -1,20 +1,17 @@
 package MP03;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.SortedMap;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         ArrayList<Lapiz> lapizs = new ArrayList<Lapiz>();
-        lapizs.add(new Lapiz(101,11));
-        lapizs.add(new Lapiz(103, 15));
-        lapizs.add(new Lapiz(106, 16));
-        lapizs.add(new Lapiz(105, 15));
+
+
+        for (int i = 0; i < 5; i++) {
+            lapizs.add(new Lapiz());
+        }
 
         System.out.println("Sin ordenar: "+ "\n");
         for (int i = 0; i <lapizs.size() ; i++) {
@@ -27,13 +24,26 @@ public class Main {
         for (int i = 0; i <lapizs.size() ; i++) {
             System.out.println(lapizs.get(i));;
         }
-
         System.out.println();
-        System.out.println("Sort por gruix :" + "\n");
-        Collections.sort(lapizs, new Sortbygruix());
 
+        // Para ELiminar:
+        System.out.println("Sort por color : ");
         System.out.println(lapizs.toString());
 
+        System.out.println();
+
+        System.out.println("Eliminar los repetidos : ");
+        Set<Lapiz> hs = new HashSet<>();
+        hs.addAll(lapizs);
+        lapizs.clear();
+        lapizs.addAll(hs);
+        System.out.println(lapizs.toString());
+
+
+        System.out.println();
+        System.out.println("Sort por gruix : ");
+        Collections.sort(lapizs, new Sortbygruix());
+        System.out.println(lapizs.toString());
 
     }
 }
